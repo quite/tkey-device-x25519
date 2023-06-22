182bd6b6b35b958d7dbec56808f6a85bd6108a9f
dehanj <hansson@tillitis.se>
2023-06-22 13:16:09 +0200

    Add execution monitor. This does change the sha512sum, and hence the key derivation

--- x25519/main.c
+++ x25519/main.c
@@ -12,6 +12,9 @@
 static volatile uint32_t *cdi =   (volatile uint32_t *)TK1_MMIO_TK1_CDI_FIRST;
 static volatile uint32_t *led =   (volatile uint32_t *)TK1_MMIO_TK1_LED;
 static volatile uint32_t *touch = (volatile uint32_t *)TK1_MMIO_TOUCH_STATUS;
+static volatile uint32_t *cpu_mon_ctrl  = (volatile uint32_t *) TK1_MMIO_TK1_CPU_MON_CTRL;
+static volatile uint32_t *cpu_mon_first = (volatile uint32_t *) TK1_MMIO_TK1_CPU_MON_FIRST;
+static volatile uint32_t *cpu_mon_last  = (volatile uint32_t *) TK1_MMIO_TK1_CPU_MON_LAST;
 
 #define LED_BLACK 0
 #define LED_RED   (1 << TK1_MMIO_TK1_LED_R_BIT)
@@ -76,6 +79,11 @@ int main(void)
 	uint8_t cmd[CMDLEN_MAXBYTES];
 	uint8_t rsp[CMDLEN_MAXBYTES];
 	uint8_t in;
+
+	// Use Execution Monitor on RAM after app
+	*cpu_mon_first = TK1_MMIO_TK1_APP_ADDR + TK1_MMIO_TK1_APP_SIZE;
+	*cpu_mon_last = TK1_RAM_BASE + TK1_RAM_SIZE;
+	*cpu_mon_ctrl = 1;
 
 	for (;;) {
 		*led = LED_GREEN | LED_BLUE;
