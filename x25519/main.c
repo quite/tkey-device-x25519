318b218029a7ee4d9871e3f9c1fafd9db2500d50
dehanj <hansson@tillitis.se>
2023-06-26 09:50:38 +0200

    Fix mistake of using pointer instead of value to exec monitor

--- x25519/main.c
+++ x25519/main.c
@@ -15,6 +15,8 @@ static volatile uint32_t *touch = (volatile uint32_t *)TK1_MMIO_TOUCH_STATUS;
 static volatile uint32_t *cpu_mon_ctrl  = (volatile uint32_t *) TK1_MMIO_TK1_CPU_MON_CTRL;
 static volatile uint32_t *cpu_mon_first = (volatile uint32_t *) TK1_MMIO_TK1_CPU_MON_FIRST;
 static volatile uint32_t *cpu_mon_last  = (volatile uint32_t *) TK1_MMIO_TK1_CPU_MON_LAST;
+static volatile uint32_t *app_addr  = (volatile uint32_t *) TK1_MMIO_TK1_APP_ADDR;
+static volatile uint32_t *app_size  = (volatile uint32_t *) TK1_MMIO_TK1_APP_SIZE;
 
 #define LED_BLACK 0
 #define LED_RED   (1 << TK1_MMIO_TK1_LED_R_BIT)
@@ -81,7 +83,7 @@ int main(void)
 	uint8_t in;
 
 	// Use Execution Monitor on RAM after app
-	*cpu_mon_first = TK1_MMIO_TK1_APP_ADDR + TK1_MMIO_TK1_APP_SIZE;
+	*cpu_mon_first = *app_addr + *app_size;
 	*cpu_mon_last = TK1_RAM_BASE + TK1_RAM_SIZE;
 	*cpu_mon_ctrl = 1;
 
